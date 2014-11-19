5ed1512d1734fa0ee08ad6bd6c1674c59b43c451
John Mulholland <jmulholland@John-iMac-2.local>
2014-11-19 21:47:00 +0000

    Red Rectangle added

--- SlotMachine/ViewController.swift
+++ SlotMachine/ViewController.swift
@@ -9,10 +9,18 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    var firstContiner: UIView!
+    var secondContainer: UIView!
+    var thirdContainer: UIView!
+    var fourthContainer: UIView!
+    
+    let kMarginForView:CGFloat = 10.0
+    let kSixth:CGFloat = 1.0/6.0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        setUpContainerViews()
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +28,12 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    func setUpContainerViews() {
+        self.firstContiner = UIView(frame: CGRect(x: self.view.bounds.origin.x + kMarginForView, y: self.view.bounds.origin.y, width: self.view.bounds.width - (kMarginForView * 2), height: self.view.bounds.height * kSixth))
+            self.firstContiner.backgroundColor = UIColor.redColor()
+            self.view.addSubview(self.firstContiner)
+    }
+    
+    
 }
 

c20468b02f45ec97c250555d47519200c41293ba
John Mulholland <jmulholland@John-iMac-2.local>
2014-11-26 18:01:29 +0000

    to updateMainView

--- SlotMachine/ViewController.swift
+++ SlotMachine/ViewController.swift
@@ -9,13 +9,15 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     var firstContainer: UIView!
     var secondContainer: UIView!
     var thirdContainer: UIView!
     var fourthContainer: UIView!
     
     var titleLabel: UILabel!
+    
+    // information labels
     
     var creditsLabel: UILabel!
     var betLabel: UILabel!
@@ -24,7 +26,22 @@ class ViewController: UIViewController {
     var betTitleLabel: UILabel!
     var winnerPaidTitleLabel: UILabel!
     
+    // buttons in Fourth Container
     
+    var resetButton: UIButton!
+    var betOneButton: UIButton!
+    var betMaxButton: UIButton!
+    var spinButton: UIButton!
+    
+    // tracking stats fields
+    var credits = 0
+    var currentBet = 0
+    var winnings = 0
+    
+    
+    var slots:[[Slot]] = []
+    
+    // constants for use with setup
     
     let kMarginForView:CGFloat = 10.0
     let kSixth:CGFloat = 1.0/6.0
@@ -33,35 +50,67 @@ class ViewController: UIViewController {
     let kNumberofSlots = 3.0
     let kThird:CGFloat = 1.0/3.0
     let kMarginForSlots:CGFloat = 2.0
+    let kHalf:CGFloat = 1.0/2.0
+    let kEighth:CGFloat = 1.0/8.0
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         setUpContainerViews()
         setupFirstContainerView(self.firstContainer)
-        setupSecondContainerView(self.secondContainer)
+        //        setupSecondContainerView(self.secondContainer)
+        //      setupSecondContainerView(self.secondContainer) is included in hardReset
+        hardReset()
         setupThirdContainerView(self.thirdContainer)
+        setupFourthContainerView(self.fourthContainer)
+        Factory.createSlots()
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
+    // IBActions
+    
+    func resetButtonPressed (button: UIButton)
+    {
+        hardReset()
+    }
+    
+    func betOneButtonPressed (button: UIButton)
+    {
+        println(button)
+    }
+    
+    func betMaxButtonPressed (button: UIButton)
+    {
+        println("Max Button pressed")
+    }
+    
+    func spinButtonPressed (button: UIButton)
+    {
+        removeSlotImageViews()
+        slots = Factory.createSlots()
+        setupSecondContainerView(self.secondContainer)
+    }
+    
+    
+    
     func setUpContainerViews() {
         self.firstContainer = UIView(frame: CGRect(x: self.view.bounds.origin.x + kMarginForView, y: self.view.bounds.origin.y, width: self.view.bounds.width - (kMarginForView * 2), height: self.view.bounds.height * kSixth))
-            self.firstContainer.backgroundColor = UIColor.redColor()
-            self.view.addSubview(self.firstContainer)
+        self.firstContainer.backgroundColor = UIColor.redColor()
+        self.view.addSubview(self.firstContainer)
         
         self.secondContainer = UIView(frame: CGRect(x: self.view.bounds.origin.x + kMarginForView, y: firstContainer.frame.height, width: self.view.bounds.width - (kMarginForView * 2), height: self.view.bounds.height * (3 * kSixth)))
-            self.secondContainer.backgroundColor = UIColor.blackColor()
-            self.view.addSubview(self.secondContainer)
+        self.secondContainer.backgroundColor = UIColor.blackColor()
+        self.view.addSubview(self.secondContainer)
         self.thirdContainer = UIView(frame: CGRect(x: self.view.bounds.origin.x + kMarginForView, y: firstContainer.frame.height + secondContainer.frame.height, width: self.view.bounds.width - (2 * kMarginForView), height: self.view.bounds.height * kSixth))
-            self.thirdContainer.backgroundColor = UIColor.lightGrayColor()
-            self.view.addSubview(self.thirdContainer)
+        self.thirdContainer.backgroundColor = UIColor.lightGrayColor()
+        self.view.addSubview(self.thirdContainer)
         self.fourthContainer = UIView(frame: CGRect(x: self.view.bounds.origin.x + kMarginForView, y: firstContainer.frame.height + self.secondContainer.frame.height + self.thirdContainer.frame.height, width: self.view.bounds.width - (2 * kMarginForView), height: self.view.bounds.height * kSixth))
-            self.fourthContainer.backgroundColor = UIColor.blackColor()
-            self.view.addSubview(fourthContainer)
+        self.fourthContainer.backgroundColor = UIColor.blackColor()
+        self.view.addSubview(fourthContainer)
         
     }
     
@@ -78,13 +127,23 @@ class ViewController: UIViewController {
     
     func setupSecondContainerView(containerView: UIView) {
         for var containerNumber = 0; containerNumber < 3; containerNumber++ {
-            for var slotNumber = 0; slotNumber < 3; slotNumber++ {
+            for var slotNumber = 0; slotNumber < 3 ; slotNumber++ {
+                
+                var slot:Slot
                 var slotImageView = UIImageView()
+                if slots.count != 0 {
+                    let slotContainer = slots[containerNumber]
+                    slot = slotContainer[slotNumber]
+                    slotImageView.image = slot.image
+                }
+                else{
+                    slotImageView.image = UIImage(named: "Ace")
+                }
                 slotImageView.backgroundColor = UIColor.yellowColor()
                 slotImageView.frame = CGRect(x: containerView.bounds.origin.x + (containerView.bounds.size.width * CGFloat(containerNumber) * kThird), y: containerView.bounds.origin.y + (containerView.bounds.size.height * CGFloat(slotNumber) * kThird), width: containerView.bounds.width * kThird - kMarginForSlots, height: containerView.bounds.height * kThird - kMarginForSlots)
                 containerView.addSubview(slotImageView)
                 
-                }
+            }
         }
     }
     
@@ -146,5 +205,80 @@ class ViewController: UIViewController {
         containerView.addSubview(self.winnerPaidTitleLabel)
     }
     
+    func setupFourthContainerView (containerView: UIView)
+    {
+        self.resetButton = UIButton()
+        self.resetButton.setTitle("Reset", forState: UIControlState.Normal)
+        self.resetButton.setTitleColor(UIColor.blueColor(), forState: UIControlState.Normal)
+        self.resetButton.titleLabel?.font = UIFont(name: "Superclarendon-Bold", size: 12)
+        self.resetButton.backgroundColor = UIColor.lightGrayColor()
+        self.resetButton.sizeToFit()
+        self.resetButton.center = CGPoint(x: containerView.frame.width * kEighth, y: containerView.frame.height * kHalf)
+        self.resetButton.addTarget(self, action: "resetButtonPressed:", forControlEvents: UIControlEvents.TouchUpInside)
+        containerView.addSubview(self.resetButton)
+        
+        
+        self.betOneButton = UIButton()
+        self.betOneButton.setTitle("Bet One", forState: UIControlState.Normal)
+        self.betOneButton.setTitleColor(UIColor.blueColor(), forState: UIControlState.Normal)
+        self.betOneButton.titleLabel?.font = UIFont(name: "Superclarendon-Bold", size: 12)
+        self.betOneButton.backgroundColor = UIColor.greenColor()
+        self.betOneButton.sizeToFit()
+        self.betOneButton.center = CGPoint(x: containerView.frame.width * kEighth * 3, y: containerView.frame.height * kHalf)
+        self.betOneButton.addTarget(self, action: "betOneButtonPressed:", forControlEvents: UIControlEvents.TouchUpInside)
+        containerView.addSubview(self.betOneButton)
+        
+        self.betMaxButton = UIButton()
+        self.betMaxButton.setTitle("Bet Max", forState: UIControlState.Normal)
+        self.betMaxButton.setTitleColor(UIColor.blueColor(), forState: UIControlState.Normal)
+        self.betMaxButton.titleLabel?.font = UIFont(name: "Superclarendon-Bold", size: 12)
+        self.betMaxButton.backgroundColor = UIColor.redColor()
+        self.betMaxButton.sizeToFit()
+        self.betMaxButton.center = CGPoint(x: containerView.frame.width * kEighth * 5, y: containerView.frame.height * kHalf)
+        self.betMaxButton.addTarget(self, action: "betMaxButtonPressed:", forControlEvents: UIControlEvents.TouchUpInside)
+        containerView.addSubview(self.betMaxButton)
+        
+        self.spinButton = UIButton()
+        self.spinButton.setTitle("Spin", forState: UIControlState.Normal)
+        self.spinButton.setTitleColor(UIColor.blueColor(), forState: UIControlState.Normal)
+        self.spinButton.titleLabel?.font = UIFont(name: "Superclarendon-Bold", size: 12)
+        self.spinButton.backgroundColor = UIColor.greenColor()
+        self.spinButton.sizeToFit()
+        self.spinButton.center = CGPoint(x: containerView.frame.width * kEighth * 7, y: containerView.frame.height * kHalf)
+        self.spinButton.addTarget(self, action: "spinButtonPressed:", forControlEvents: UIControlEvents.TouchUpInside)
+        containerView.addSubview(self.spinButton)
+        
+    }
+    
+    func removeSlotImageViews()
+    {
+        if self.secondContainer != nil {
+            let container:UIView? = self.secondContainer
+            let subViews:Array? = container!.subviews
+            for view in subViews! {
+                view.removeFromSuperview()
+            }
+        }
+    }
+    
+    func hardReset()
+        
+    {
+        slots.removeAll(keepCapacity: true)
+        removeSlotImageViews()
+        credits = 50
+        winnings = 0
+        currentBet = 0
+        updateMainView()
+    }
+    
+    func updateMainView()
+        
+    {
+        self.creditsLabel.text = "\(credits)"
+        self.betLabel.text = "\(currentBet)"
+        self.winnerPaidLabel.text = "\(winnings)"
+        
+    }
 }
 
